aced852f5b0e1afbd91b07501f3a802d1c6341fa
Mateus Henrique Nascimento Soares <mateushenreiquensoares@gmail.com>
2020-06-25 07:23:25 -0300

    Base of Extension

--- src/extension.ts
+++ src/extension.ts
@@ -8,10 +8,33 @@ export function activate(context: vscode.ExtensionContext) {
 
   vscode.commands.registerCommand(
     'mit-license-generator.generate',
-    () => {
-      vscode.window.showInformationMessage('Generating...')
+    (target: vscode.Uri) => {
+      console.log(target)
+      console.log(license)
+      console.log(
+        String(
+          vscode.workspace.getConfiguration().get('mit-license-generator.name')
+        )
+      )
+      vscode.workspace.fs.writeFile(
+        vscode.Uri.parse(`${target}/LICENSE`),
+        Buffer.from(
+          license
+            .join('\n')
+            .replace(/<YEAR>/, String(new Date().getFullYear()))
+            .replace(
+              /<NAME>/,
+              String(
+                vscode.workspace
+                  .getConfiguration()
+                  .get('mit-license-generator.name') ||
+                  'Replace it by your name or configure mit-license-generator.name in settings.json'
+              )
+            )
+        )
+      )
     }
   )
 }
 
-export function deactivate() { }
+export function deactivate() {}
